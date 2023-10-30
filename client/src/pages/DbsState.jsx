1dcbfc514a6de5aa183109dd20d2c2ef64b075c8
PhoebeJChang <phoebechang00@gmail.com>
2023-10-31 00:01:36 +0800

    Ag-grid add

--- client/src/pages/DbsState.jsx
+++ client/src/pages/DbsState.jsx
@@ -1,9 +1,149 @@
-// import React from 'react'
+import React, { useState, useRef, useEffect, useMemo, useCallback } from 'react';
+import { createRoot } from 'react-dom/client';
+import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
+
+import 'ag-grid-community/styles/ag-grid.css'; // Core grid CSS, always needed
+import 'ag-grid-community/styles/ag-theme-alpine.css'; // Optional theme CSS
 
 const DbsState = () => {
+
+  const gridRef = useRef(); // Optional - for accessing Grid's API
+  const [rowData, setRowData] = useState(); // Set rowData to Array of Objects, one Object per Row
+
+  // Each Column Definition results in one Column.
+  const [columnDefs, setColumnDefs] = useState([
+    { field: 'make', filter: true },
+    { field: 'model', filter: true },
+    { field: 'price' }
+  ]);
+
+  // DefaultColDef sets props common to all Columns
+  const defaultColDef = useMemo(() => ({
+    sortable: true
+  }));
+
+  // Example of consuming Grid Event
+  const cellClickedListener = useCallback(event => {
+    console.log('cellClicked', event);
+  }, []);
+
+  // Example load data from server
+  useEffect(() => {
+    fetch('https://www.ag-grid.com/example-assets/row-data.json')
+      .then(result => result.json())
+      .then(rowData => setRowData(rowData))
+  }, []);
+
+  // Example using Grid's API
+  const buttonListener = useCallback(e => {
+    gridRef.current.api.deselectAll();
+  }, []);
+
   return (
-    <h1>Dbs State Page</h1>
-  )
+    <div>
+
+      {/* Example using Grid's API */}
+      {/* <button onClick={buttonListener}>Push Me</button> */}
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        paddingBlockEnd: 20
+      }} >
+        <h2>MongoDB</h2>
+      </div>
+
+
+      {/* On div wrapping Grid a) specify theme CSS Class Class and b) sets Grid size */}
+      <div className="ag-theme-alpine" style={{
+        width: '100%',
+        height: 500,
+        justifyContent: 'center',
+      }}>
+
+        <AgGridReact
+          ref={gridRef} // Ref for accessing Grid's API
+
+          rowData={rowData} // Row Data for Rows
+
+          columnDefs={columnDefs} // Column Defs for Columns
+          defaultColDef={defaultColDef} // Default Column Properties
+
+          animateRows={true} // Optional - set to 'true' to have rows animate when sorted
+          rowSelection='multiple' // Options - allows click selection of rows
+
+          onCellClicked={cellClickedListener} // Optional - registering for Grid Event
+        />
+      </div>
+
+      <div>
+        <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          paddingTop: 20,
+          paddingBlockEnd: 20,
+        }} >
+          <h2>MySQL DB</h2>
+        </div>
+        <div className="ag-theme-alpine" style={{
+          width: '100%',
+          height: 500,
+          justifyContent: 'center',
+        }}>
+
+          <AgGridReact
+            ref={gridRef} // Ref for accessing Grid's API
+
+            rowData={rowData} // Row Data for Rows
+
+            columnDefs={columnDefs} // Column Defs for Columns
+            defaultColDef={defaultColDef} // Default Column Properties
+
+            animateRows={true} // Optional - set to 'true' to have rows animate when sorted
+            rowSelection='multiple' // Options - allows click selection of rows
+
+            onCellClicked={cellClickedListener} // Optional - registering for Grid Event
+          />
+        </div>
+      </div>
+
+
+      <div>
+        <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          paddingTop: 20,
+          paddingBlockEnd: 20,
+        }} >
+          <h2>Azure SQL</h2>
+        </div>
+        <div className="ag-theme-alpine" style={{
+          width: '100%',
+          height: 500,
+          justifyContent: 'center',
+        }}>
+
+          <AgGridReact
+            ref={gridRef} // Ref for accessing Grid's API
+
+            rowData={rowData} // Row Data for Rows
+
+            columnDefs={columnDefs} // Column Defs for Columns
+            defaultColDef={defaultColDef} // Default Column Properties
+
+            animateRows={true} // Optional - set to 'true' to have rows animate when sorted
+            rowSelection='multiple' // Options - allows click selection of rows
+
+            onCellClicked={cellClickedListener} // Optional - registering for Grid Event
+          />
+        </div>
+      </div>
+
+
+    </div>
+
+
+  );
+
 }
 
 export default DbsState
