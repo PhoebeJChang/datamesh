71c54a1511644061c1c6418f6f81a261f150f303
PhoebeJChang <phoebechang00@gmail.com>
2023-11-22 22:27:15 +0800

    add toastify in client

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import customFetch from '../src/utils/customFetch.js'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
 // fetch('http://localhost:5100/api/v1/test')
 // fetch('/api/v1/test')
 //   .then((res) => res.json())
 //   .then((data) => console.log(data));
-const resp = await customFetch.get('/basicInfo');
-console.log(resp);
+// const resp = await customFetch.get('/basicInfo');
+// console.log(resp);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ToastContainer position='top-center' />
   </React.StrictMode>,
 )
