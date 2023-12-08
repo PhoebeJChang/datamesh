5ce1c9dc07ad4664cead5d1723b6aa58198e4936
wendy <69668416+WendyPan90@users.noreply.github.com>
2023-12-08 23:38:22 +0800

    add note

--- client/src/pages/AllBasicInfos.jsx
+++ client/src/pages/AllBasicInfos.jsx
@@ -1,111 +1,53 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { toast } from 'react-toastify';
 import { BasicInfosContainer, SearchContainer } from '../components';
 import customFetch from '../utils/customFetch';
 import { useLoaderData } from 'react-router-dom';
 import { useContext, createContext, useEffect, useState} from 'react';
-import axios from 'axios';
-//import { useInfiniteQuery } from '@tanstack/react-query';
-
-const BasicInfoList = () => {
-  const [basicInfos, setBasicInfos] = useState([]);
-  const [page, setPage] = useState(1);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`/basicInfo?page=${page}&limit=${limit}`);
-        const newData = response.data.data;
-
-        setBasicInfos((prevData) => [...prevData, ...newData]);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, [page]);
-
-  const handleScroll = () => {
-    // 檢查滾動到底部的邏輯
-    if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) {
-      setPage((prevPage) => prevPage + 1);
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  return (
-    <div className="basicInfoList">
-      {basicInfos.map((basicInfo) => (
-        <BasicInfo key={basicInfo._id} {...basicInfo} />
-      ))}
-    </div>
-  );
-};
 
 
+//const [basicInfos, setBasicInfos] = useState([]);
+//const [page, setPage] = useState(1);
+var page = 1;
+const limit = 4;
 
-//mock the database fetch
-/*const fetchData = async (page: number) =>{
-  await new Promise((resolve) => setTimeout(resolve,1000))
-  return basicdata //right here shoud limit the page to load
-  //return data.slice((page -1)*2, page*2)
-}
-
-//infinite query
-const Page = () => {
-  const { data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery( //query is a unique identifier for hook
-    ['query'] = asyn ({ pageParam = 1}) => {
-      const response = await fetchData(pageParam)
-      return response
-    },
-    {
-      getNextPageParam : (_, pages) => {
-        return pages.length + 1
-      }
-    },
-    {
-      initialDate: {
-        pages: [data.slice(0,10)] //right here should dafault 10 data to view
-        pagePrarms: [1] //set pageparams to first page 1
-      }
-    }
-  )
-}*/
-
+//get server data
 export const loader = async () => {
+  //get data   
   try {
-    //await new Promise((resolve) => setTimeout(resolve,1000))
-    const { data } = await customFetch.get('/basicInfo');
-    return {
-      data
-    };
+    var data = await customFetch.get(`/basicInfo?page=${page}&limit=${limit}`); //get data with page and limit
+    if(page > 1){
+      const newData = await customFetch.get(`/basicInfo?page=${page}&limit=${limit}`);
+      data = data.push(newData) //merge data
+    }
+    return data
+    //const newData = response.data; //store new data
+    //setBasicInfos((prevData) => [...prevData, ...newData]); //merge latest data
   } catch (error) {
     toast.error(error?.response?.data?.msg);
     return error;
   }
-};
-/*
-return (
-  <div>
-        posts:
-          {basicdata?.page.map((page, i) => (
-            <div key={i}>
-              {page.map((basicdata) => (
-                <div key={basicdata.medical_history_no}></div>
-              ))}
-              </div>
-          ))}
-          <button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
-            {isFetchingNextPage? 'Loading more...': (basicdata?.page.length ?? 0) < 1000? 'Load More':'Noting more to load'}
-          </button>
-      </div>
-)*/
+}
+
+//pending windows scroll
+// const handleScroll = () => {
+//   if (
+//     window.innerHeight + window.scrollY >= document.body.offsetHeight - 100 //window's height
+//   ) {
+//     setPage((prevPage) => prevPage + 1); //add page(change page status: pevPage
+//     loader(); //call loader to load data
+//   }
+// };
+
+
+//problem here
+//  useEffect(() => { //callback function
+//   console.log('it is doing useEffect()')
+//  }, []); //if page change, then do the callback function
+
+
+
+
 
 const AllBasicInfosContext = createContext();
 
@@ -124,3 +66,4 @@ export const useAllBasicInfosContext = () => useContext(AllBasicInfosContext)
 
 
 export default AllBasicInfos
+//export default BasicInfoList
