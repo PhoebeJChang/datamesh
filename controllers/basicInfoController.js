0c31eb1259f2efd658708ded58c0055919c2992c
PhoebeJChang <phoebechang00@gmail.com>
2023-12-12 22:50:25 +0800

    controller sort backend

--- controllers/basicInfoController.js
+++ controllers/basicInfoController.js
@@ -3,9 +3,23 @@ import { StatusCodes } from 'http-status-codes';
 
 //Get all BasicInfo
 export const getAllBasicInfos = async (req, res) => {
-  // console.log(req)
-  // find({ history_recorder: req.user.userId })
-  const basicInfos = await BasicInfo.find({ });
+  const { search } = req.query;
+
+  const queryObject = {
+    //only show the info created by the current user
+    // history_recorder: req.user.userId,
+  }
+
+  if (search) {
+    //'&or' from mongo syntax
+    queryObject.$or = [
+      { id_number: { $regex: search, $options: 'i' } },
+    ]
+  }
+
+  // console.log(req.query)
+  const basicInfos = await BasicInfo.find(queryObject).sort('medical_history_no');
+  //use'-medical_history_no', if it is opposite
   res.status(StatusCodes.OK).json({ basicInfos });
 };
 
@@ -15,18 +29,18 @@ export const createBasicInfo = async (req, res) => {
   console.log("reqqq", req.user)
   req.body.history_recorder = req.user.userId;
   const basicInfo = await BasicInfo.create(req.body);
-  res.status(StatusCodes.CREATED).json({ msg: 'Patient Created'});
+  res.status(StatusCodes.CREATED).json({ msg: 'Patient Created' });
 }
 
 // GET SINGLE BasicInfo
 export const getBasicInfo = async (req, res) => {
-  const basicInfo = await BasicInfo.findOne({medical_history_no: req.params.medical_history_no})  
+  const basicInfo = await BasicInfo.findOne({ medical_history_no: req.params.medical_history_no })
   res.status(StatusCodes.OK).json({ basicInfo });
 }
 
 // EDIT BasicInfo
 export const updateBasicInfo = async (req, res) => {
-  const updatedBasicInfo = await BasicInfo.findOneAndUpdate({medical_history_no: req.params.medical_history_no}, req.body, {
+  const updatedBasicInfo = await BasicInfo.findOneAndUpdate({ medical_history_no: req.params.medical_history_no }, req.body, {
     new: true
   })
   res.status(StatusCodes.OK).json({ msg: 'BasicInfo Updated' });
@@ -34,6 +48,6 @@ export const updateBasicInfo = async (req, res) => {
 
 // DELETE BasicInfo
 export const deleteBasicInfo = async (req, res) => {
-  const removeBasicInfo = await BasicInfo.findOneAndDelete({medical_history_no: req.params.medical_history_no});
-  res.status(StatusCodes.OK).json({ msg: 'BasicInfo Deleted'});
+  const removeBasicInfo = await BasicInfo.findOneAndDelete({ medical_history_no: req.params.medical_history_no });
+  res.status(StatusCodes.OK).json({ msg: 'BasicInfo Deleted' });
 }
