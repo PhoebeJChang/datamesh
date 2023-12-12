09c9d39e7404816eeefc59467006acdffc331e9b
PhoebeJChang <phoebechang00@gmail.com>
2023-12-13 02:37:13 +0800

    add pagination container

--- client/src/components/BasicInfosContainer.jsx
+++ client/src/components/BasicInfosContainer.jsx
@@ -2,21 +2,24 @@
 import BasicInfo from './BasicInfo'
 import Wrapper from '../assets/wrappers/BasicInfosContainer'
 import { useAllBasicInfosContext } from '../pages/AllBasicInfos'
+import PageBtnContainer from './PageBtnContainer'
 
 const BasicInfosContainer = () => {
   const { data } = useAllBasicInfosContext();
-  const { basicInfos } = data;
+  const { basicInfos, totalBasicInfos, numOfPages } = data;
   if (basicInfos.length === 0) {
     return <Wrapper>
       <h2>No basic infos to display...</h2>
     </Wrapper>
   }
   return <Wrapper>
+    <h5>共{totalBasicInfos}筆 病人基本資料{basicInfos.length > 1 }</h5>
     <div className="basicInfos">
       {basicInfos.map((basicInfo) => {
         return <BasicInfo key={basicInfo.medical_history_no} {...basicInfo} />
       })}
     </div>
+    {numOfPages > 1 && <PageBtnContainer/>}
   </Wrapper>
 }
 
---
+++ client/src/components/PageBtnContainer.jsx
@@ -0,0 +1,12 @@
+import { HiChevronDoubleLeft, HiChevronDoubleRight } from 'react-icons/hi';
+import Wrapper from '../assets/wrappers/PageBtnContainer';
+import { useLocation, Link, useNavigate } from 'react-router-dom';
+import { useAllBasicInfosContext } from '../pages/AllBasicInfos'
+
+const PageBtnContainer = () => {
+  return (
+    <h1>PageBtnContainer</h1>
+  )
+}
+
+export default PageBtnContainer
