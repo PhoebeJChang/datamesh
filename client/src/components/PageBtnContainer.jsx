418d3ab72e69295ceec8dbef9bc6504d87a786ee
PhoebeJChang <phoebechang00@gmail.com>
2023-12-13 03:16:02 +0800

    page btn logic and UI

--- client/src/components/PageBtnContainer.jsx
+++ client/src/components/PageBtnContainer.jsx
@@ -4,9 +4,66 @@ import { useLocation, Link, useNavigate } from 'react-router-dom';
 import { useAllBasicInfosContext } from '../pages/AllBasicInfos'
 
 const PageBtnContainer = () => {
-  return (
-    <h1>PageBtnContainer</h1>
-  )
-}
+  const {
+    data: { numOfPages, currentPage },
+  } = useAllBasicInfosContext();
+  // const { search, pathname } = useLocation();
+  // const navigate = useNavigate();
+  const pages = Array.from({ length: numOfPages }, (_, index) => {
+    return index + 1
+  });
+  
+  const { search, pathname } = useLocation();
+  const navigate = useNavigate();
 
-export default PageBtnContainer
+  const handlePageChange = (pageNumber) => {
+    console.log("page num", pageNumber)
+    const searchParams = new URLSearchParams(search);
+    searchParams.set('page', pageNumber);
+    navigate(`${pathname}?${searchParams.toString()}`);
+  };
+
+  return (
+    <Wrapper>
+      <button
+        className='btn prev-btn'
+        onClick={() => {
+          let prevPage = currentPage - 1;
+          if (prevPage < 1) {
+            prevPage = numOfPages;
+          }
+          handlePageChange(prevPage);
+        }}
+      >
+        <HiChevronDoubleLeft />
+        prev
+      </button>
+      <div className='btn-container'>
+        {pages.map((pageNumber) => (
+          <button
+            className={`btn page-btn ${pageNumber === currentPage && 'active'}`}
+            key={pageNumber}
+            onClick={() => handlePageChange(pageNumber)}
+          >
+            {pageNumber}
+          </button>
+        ))}
+      </div>
+      <button
+        className='btn next-btn'
+        onClick={() => {
+          let nextPage = currentPage + 1;
+          if (nextPage > numOfPages) {
+            nextPage = 1;
+          }
+          handlePageChange(nextPage);
+        }}
+      >
+        next
+        <HiChevronDoubleRight />
+      </button>
+    </Wrapper>
+  );
+};
+
+export default PageBtnContainer;
