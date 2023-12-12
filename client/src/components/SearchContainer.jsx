2a9c4693456747a7762b2e7683f0eae4ab0af4b2
PhoebeJChang <phoebechang00@gmail.com>
2023-12-13 01:08:39 +0800

    UI&logic of search sontainer

--- client/src/components/SearchContainer.jsx
+++ client/src/components/SearchContainer.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { FormRow, FormRowSelect, SubmitBtn } from '.';
+import Wrapper from '../assets/wrappers/DashboardFormPage';
+import { Form, useSubmit, Link } from 'react-router-dom';
+import { BASICINFO_SORT_BY } from '../../../utils/constance';
+import { useAllBasicInfosContext } from '../pages/AllBasicInfos'
 
 const SearchContainer = () => {
   return (
-    <h2>SearchContainer</h2>
+    <Wrapper>
+      <Form className='form'>
+        <h5 className='form-title'>search form</h5>
+        <div className='form-center'>
+          {/* search position */}
+
+          <FormRow type='search' name='search' defaultValue='請搜尋身分證字號' />
+          <FormRowSelect
+            name='sort'
+            defaultValue='ascending'
+            list={[...Object.values(BASICINFO_SORT_BY)]}
+          />
+
+          <Link to='/dashboard/all-basicinfos' className='btn form-btn delete-btn'>
+            重整搜尋項目
+          </Link>
+          {/* TEMP!!!! */}
+          <SubmitBtn formBtn />
+        </div>
+      </Form>
+    </Wrapper>
   )
 }
 
