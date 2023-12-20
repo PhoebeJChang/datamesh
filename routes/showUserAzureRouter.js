a784f7b98e2f8af07602c751c12195d08e9d6ae1
PhoebeJChang <phoebechang00@gmail.com>
2023-12-21 03:55:40 +0800

    add azure router

---
+++ routes/showUserAzureRouter.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+// import { 
+//   getAllUsers, 
+//   getUser, 
+//   updateUser, 
+//   deleteUser
+// } from "../controllers/authController.js";
+// import {  validateIdParams} from "../middleware/validationMiddleware.js";
+const router = Router();
+
+
+// router.route('/').get(getAllUsers)
+
+// router
+//   .route('/:id')
+//   .get(validateIdParams, getUser)
+//   .patch(updateUser)
+//   .delete(validateIdParams, deleteUser)
+
+export default router;
