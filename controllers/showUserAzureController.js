9042a14a8ba874ccb04ae97be573ab9f2cb76196
PhoebeJChang <phoebechang00@gmail.com>
2023-12-21 22:54:48 +0800

    updated azure controller

--- controllers/showUserAzureController.js
+++ controllers/showUserAzureController.js
@@ -12,50 +12,58 @@ export const getAllUsers = async (req, res) => {
    *********************************/
   const { search, sort } = req.query;
 
-  const queryObject = {
-    //only show the info created by the current user
-    // history_recorder: req.user.userId,
-  }
+  // Define the base SQL query
+  let sqlQuery = "SELECT * FROM users WHERE 1=1"; // Start with a basic query
 
-  if (search) {
-    // Convert the search string to a number for querying
+   // Add searching
+   if (search) {
+    // Customize the search criteria based on your requirements
     const numericSearch = parseInt(search);
-
     if (!isNaN(numericSearch)) {
-      // If successfully converted the search string to a number, use it in the query
-      queryObject.id = numericSearch;
+      sqlQuery += ` AND id = ${numericSearch}`;
     } else {
-      // If unable to convert the search string to a number, you can handle errors or perform other actions
+      sqlQuery += ` AND (name LIKE '%${search}%' OR email LIKE '%${search}%')`;
     }
   }
 
+  // Define sort options
   const sortOptions = {
-    ascending: 'id',
-    descending: '-id',
+    ascending: 'id ASC',
+    descending: 'id DESC',
   };
 
   //if client didn't select specific way, default will
+  // Determine the sort key
   const sortKey = sortOptions[sort] || sortOptions.ascending;
+
+  // Add sorting
+  sqlQuery += ` ORDER BY ${sortKey}`;
 
   /*********************************
    * setup for pagination
    *********************************/
-  var totalusers = 0;
-
   // get the current page from client but the default is page 1
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 20;
   const skip = (page - 1) * limit;
 
-  /*********************************
+  // Calculate the total number of records without applying limit and offset
+  const countQuery = `SELECT COUNT(*) AS total FROM users WHERE 1=1${search ? ` AND (name LIKE '%${search}%' OR email LIKE '%${search}%')` : ''}`;
+  const countResult = await request.query(countQuery);
+
+  const totalusers = countResult.recordset[0].total;
+  const numOfPages = Math.ceil(totalusers / limit);
+
+  // Add pagination (LIMIT and skip) to the main SQL query
+  sqlQuery += ` OFFSET ${skip} ROWS FETCH NEXT ${limit} ROWS ONLY`;
+
+  /********************************* 
    * send back json response
    *********************************/
 
-  request.query("select * from users", (err, result) => {
+  request.query(sqlQuery, (err, result) => {
     if (err) throw err;
     const userData = result.recordset;
-    totalusers = userData.length
-    const numOfPages = Math.ceil(totalusers / limit);
     res.status(StatusCodes.OK).json({ totalusers, numOfPages, currentPage: page, users: userData })
   });
 }
